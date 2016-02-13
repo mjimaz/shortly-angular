6c9a49c0a85a6d23d80a0328d35249aefc982626
mjimaz <m.aljimaz@gmail.com>
2016-02-12 16:36:03 -0800

    Complete LinksController functionality, add scope.data.links property that holds all links

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,9 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
+
+  Links.getAll().then(function (links) {
+    $scope.data.links = links;
+  });
+
 });
