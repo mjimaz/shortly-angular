33513e07aa4bebf912bb488515727b148dea5d84
mjimaz <m.aljimaz@gmail.com>
2016-02-12 20:08:45 -0800

    Add link-displaying functionalities to links.html

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,10 +1,12 @@
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+  // Not currently using $scope.link
   $scope.link = {};
+  
   $scope.addLink = function (url) {
     console.log(url);
     Links.addOne({url: url});
+    $scope.url = '';
   };
 });
