d2a3e586ea4dc535507742dcafe25a4b5e500dea
mjimaz <m.aljimaz@gmail.com>
2016-02-13 15:48:34 -0800

    clean code

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,8 +2,7 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   // Not currently using $scope.link
-  $scope.link = {};
-  
+  // $scope.link = {};
   $scope.addLink = function (url) {
     console.log(url);
     Links.addOne({url: url});
